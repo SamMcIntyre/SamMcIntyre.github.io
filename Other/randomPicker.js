8bbc298f7d34272996563fe20ffe6eedeeab6f6d
SamMcIntyre <42543485+SamMcIntyre@users.noreply.github.com>
2018-12-01 16:34:55 -0600

    Update randomPicker.js

--- Other/randomPicker.js
+++ Other/randomPicker.js
@@ -2,7 +2,7 @@
 
 var choices = ["San Jose", "Qdoba", "Helens"];
 
-var pick = Math.floor(Math.random()*choices.length-1);
+var pick = Math.floor(Math.random()*choices.length);
 
 document.getElementById("vote").textContent = choices[pick];
 
